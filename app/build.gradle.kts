aa47b25a7172ce9cba897d1b533ef8f9400d9f3c
GitHub <PixelXpert@siava.sh>
2026-03-20 14:08:55 +0000

    Version update: Release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
 		applicationId = "sh.siava.pixelxpert"
 		minSdk = 36
 		targetSdk = 36
-		versionCode = 489
-		versionName = "canary-489"
+		versionCode = 490
+		versionName = "canary-490"
 		ndk {
 			//noinspection ChromeOsAbiSupport
 			abiFilters.add("arm64-v8a")
