de4d779a3336dd8a89c34208e045654cd6a0a266
Lars Martens <larsmartens@users.noreply.github.com>
2026-04-24 14:18:59 +0200

    fix: restore missing scanner import

--- app/src/main/java/sh/siava/pixelxpert/ui/fragments/UpdateFragment.java
+++ app/src/main/java/sh/siava/pixelxpert/ui/fragments/UpdateFragment.java
@@ -49,6 +49,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Scanner;
 import java.util.function.Consumer;
 
 import sh.siava.pixelxpert.BuildConfig;
