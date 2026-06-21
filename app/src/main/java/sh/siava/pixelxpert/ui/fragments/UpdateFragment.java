56c4714b25ecfa68471abc9d4bd4ea390cc5900e
lars-martens <lars@mrtns.org>
2026-06-21 16:23:23 +0000

    Fix missing imports in UpdateFragment changelog fetch
    Consumer and Scanner were used in fetchChangelog() without imports,
    breaking the build.

--- app/src/main/java/sh/siava/pixelxpert/ui/fragments/UpdateFragment.java
+++ app/src/main/java/sh/siava/pixelxpert/ui/fragments/UpdateFragment.java
@@ -49,6 +49,8 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Scanner;
+import java.util.function.Consumer;
 
 import sh.siava.pixelxpert.BuildConfig;
 import sh.siava.pixelxpert.PixelXpert;
